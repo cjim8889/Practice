75b6b7364930ad8175c207a0e889a507b682f932
Wuhao Chen <c.jim8889@outlook.com>
2019-02-20 20:54:53 +0000

    LWL

---
+++ LastWordLength.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLastWord(self, s: 'str') -> 'int':
+        if (len(s) == 0):
+            return 0
+
+
+        length = 0
+        lengthS = len(s)
+
+        flag = False
+        for i in range(lengthS):
+            if (s[lengthS - 1 - i] != ' '):
+                flag = True
+                length += 1
+            else:
+                if (flag)：
+                    break
+        return length
+
+
+if __name__ == "__main__":
+    Solution().lengthOfLastWord("Hello World")
